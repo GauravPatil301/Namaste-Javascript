b321568e2e55263aa2227f17aabe45d3e81d3359
GauravPatil301 <102862547+GauravPatil301@users.noreply.github.com>
2023-09-12 11:26:36 +0530

    Time: 66 ms (54.96%) | Memory: 43.4 MB (98.44%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.replace(/[^a-zA-Z0-9]/g,'').toLowerCase();
+    const len = s.length
+    for(let i=0;i<len/2;i++){
+        if(s[i] !== s[len-i-1]){
+            return false
+        }
+    }
+    return true
+};
